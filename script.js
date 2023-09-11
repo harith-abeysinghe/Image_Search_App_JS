4011921169600737a8d201af07e45f8b15e73d6a
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-09-11 21:57:11 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const access_key = "_leSm6EuqT4K-CXcwqPMJuKd_2r4D6FlknVf_a-6Ug0"
+
+const formInput = document.querySelector("form")
+const input = document.getElementById("search_input")
+
+const search_results = document.querySelector(".seacrh_results")
+
+const show_more = document.getElementById("show_more_button")
